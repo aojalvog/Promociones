afb3dcac7faf92662196abdf5ec8f337d38726fc
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-07 15:05:10 +0200

    feat(Promociones): :sparkles: Creación del método process

--- src/main/java/com/springbatch/proyectoPromociones/process/StockProcessor.java
+++ src/main/java/com/springbatch/proyectoPromociones/process/StockProcessor.java
@@ -1,5 +1,14 @@
 package com.springbatch.proyectoPromociones.process;
 
-public class StockProcessor {
+import org.springframework.batch.item.ItemProcessor;
+
+import com.springbatch.proyectoPromociones.model.Productos;
+
+public class StockProcessor implements ItemProcessor<Productos, Productos> {
+
+	@Override
+	public Productos process(Productos item) throws Exception {
+		return item;
+	}
 
 }
